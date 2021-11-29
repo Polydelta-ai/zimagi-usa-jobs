2d7e0b5a54f1dfc8f80ee3dd303884f0f06adeeb
Adrian Webb <adrian@webb.sh>
2021-11-28 21:54:37 -0500

    Refactoring the USA Jobs API utility library to support historical announcements.

--- utility/usa_jobs_api.py
+++ utility/usa_jobs_api.py
@@ -63,22 +63,43 @@ class USAJobsCodeListResponse(USAJobsResponse):
             self._results.append(RecursiveCollection(**self._convert_keys(result)))
 
 
+class USAJobsAnnouncementResponse(USAJobsResponse):
+
+    def __init__(self, response):
+        super().__init__(response)
+
+        self._paging = self.json.get('paging', {}).get('metadata', {})
+        self._data = self.json.get('data', [])
+
+        for announcement in self._data:
+            self._results.append(RecursiveCollection(**self._convert_keys(announcement)))
+
+    @property
+    def count(self):
+        return len(self._data)
+
+    @property
+    def full_count(self):
+        return self._paging.get('totalCount', 0)
+
+
 class USAJobsSearchResponse(USAJobsResponse):
 
     def __init__(self, response):
         super().__init__(response)
 
         self._top = self.json.get('SearchResult', {})
+        self._data = self._top.get('SearchResultItems', [])
 
-        for result in self._top.get('SearchResultItems', []):
+        for result in self._data:
             job = result['MatchedObjectDescriptor']
             job['UserArea'] = job['UserArea']['Details']
 
             self._results.append(RecursiveCollection(**self._convert_keys(job)))
 
     @property
-    def page_count(self):
-        return self._top.get('SearchResultCount', 0)
+    def count(self):
+        return len(self._data)
 
     @property
     def full_count(self):
@@ -90,9 +111,8 @@ class USAJobsAPI(object):
     BASE_URL = "https://data.usajobs.gov"
 
 
-    def __init__(self, command, page_count = 500, wait_time = 0.5):
+    def __init__(self, command, wait_time = 0.5):
         self.command = command
-        self.page_count = page_count
         self.wait_time = wait_time
 
         self.api_email = settings.USA_JOBS_API_EMAIL
@@ -103,31 +123,6 @@ class USAJobsAPI(object):
         response = self._codelist(name)
         for result in response.results:
             yield result
-
-
-    def search(self, params = None):
-        response = None
-        next_page = 1
-
-        if params is None:
-            params = {}
-        else:
-            params = copy.deepcopy(params)
-
-        for name, value in params.items():
-            if isinstance(value, (list, tuple)):
-                params[name] = ";".join(value)
-
-        while response is None or response.page_count == self.page_count:
-            params['Page'] = next_page
-            response = self._search(params)
-
-            for result in response.results:
-                yield result
-
-            next_page += 1
-            time.sleep(self.wait_time)
-
 
     def _codelist(self, name):
         self.command.info("Querying USA Jobs codes for: {}".format(name))
@@ -140,14 +135,56 @@ class USAJobsAPI(object):
         return USAJobsCodeListResponse(response)
 
 
-    def _search(self, params):
+    def announcements(self, page_count = 1000, **params):
+        response = None
+        next_page = 1
+
+        while response is None or response.count == page_count:
+            response = self._announcements(params, page_count, next_page)
+
+            for result in response.results:
+                yield result
+
+            next_page += 1
+            time.sleep(self.wait_time)
+
+    def _announcements(self, params, page_count, page):
+        params['Pagesize'] = page_count
+        params['PageNumber'] = page
+
+        self.command.info("Searching USA Job announcements with search parameters: {}".format(params))
+        response = requests.get("{}/api/historicjoa".format(self.BASE_URL))
+        logger.debug(response.url)
+
+        if response.status_code != 200:
+            raise USAJobsException("USA Jobs Announcement API returned status code: {}".format(response.status_code))
+
+        return USAJobsAnnouncementResponse(response)
+
+
+    def search(self, page_count = 500, **params):
+        response = None
+        next_page = 1
+
+        for name, value in params.items():
+            if isinstance(value, (list, tuple)):
+                params[name] = ";".join(value)
+
+        while response is None or response.count == page_count:
+            response = self._search(params, page_count, next_page)
+
+            for result in response.results:
+                yield result
+
+            next_page += 1
+            time.sleep(self.wait_time)
+
+    def _search(self, params, page_count, page):
         if not self.api_email or not self.api_key:
             raise USAJobsException('Environment variables ZIMAGI_USA_JOBS_API_EMAIL and ZIMAGI_USA_JOBS_API_KEY required with your credentials')
 
-        params['ResultsPerPage'] = self.page_count
-
-        if 'Page' not in params:
-            params['Page'] = 1
+        params['ResultsPerPage'] = page_count
+        params['Page'] = page
 
         self.command.info("Searching USA Jobs with search parameters: {}".format(params))
         response = requests.get("{}/api/search".format(self.BASE_URL), params = params, headers = {
